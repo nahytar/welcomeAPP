3d3e136dbfc6c9fd4dce4fd2614ae596a4fedba5
nahytar <nahytar@gmail.com>
2018-09-23 12:38:45 -0300

    se crean componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
