c5b9c452cc2f8e4f974b77a6ead9a89ba897606a
nahytar <nahytar@gmail.com>
2018-09-23 14:11:04 -0300

    se añade navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { MapaComponent } from './componentes/mapa/mapa.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'mapa', component: MapaComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePageComponent } from './componentes/home-page/home-page.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { LoginPageComponent } from './componentes/login-page/login-page.component';
 import { ProfileComponent } from './componentes/profile/profile.component';
+import { MapaComponent } from './componentes/mapa/mapa.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProfileComponent } from './componentes/profile/profile.component';
     HomePageComponent,
     NavbarComponent,
     LoginPageComponent,
-    ProfileComponent
+    ProfileComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
