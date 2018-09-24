c6e7d57747513a62b227a3489aea3e494ec9e32a
CaritoDasilva <caritodasilva@gmail.com>
2018-09-24 02:16:56 -0300

    Se agrega decorador al componente de home

--- src/app/componentes/home-page/home-page.component.ts
+++ src/app/componentes/home-page/home-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
+import { EventEmitter } from 'events';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+    @Input() post: any = {}
+    @Output() postAgregado: EventEmitter;
 
-  constructor() { }
+    constructor() {
+        this.postAgregado = new EventEmitter();
+    }
 
-  ngOnInit() {
-  }
+
+    ngOnInit() {
+    }
+    createPost() {
+
+    }
+    addPost() {
+        this.postAgregado.emit(this.post);
+        // let postNew = {
+        //     content: this.memeForm.value.description,
+        //     likes: 0,
+        //     key$: string
+        // }
+    }
 
 }
