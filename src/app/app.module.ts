52c8c6a55b1efed3f330b30283aa7836aa133544
CaritoDasilva <caritodasilva@gmail.com>
2018-09-26 14:49:15 -0300

    Se agrega flexbox al muro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,10 @@ import { MapaComponent } from './componentes/mapa/mapa.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/home-page/home-page.component.ts
+++ src/app/componentes/home-page/home-page.component.ts
@@ -1,28 +1,42 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { post } from 'selenium-webdriver/http';
 import { EventEmitter } from 'events';
+
+
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss']
 })
+
 export class HomePageComponent implements OnInit {
-    @Input() post: any = {}
-    @Output() postAgregado: EventEmitter;
+    post: FormGroup;
+
+    // @Input() post: any = {}
+    // @Output() postAgregado: EventEmitter;
 
     constructor() {
-        this.postAgregado = new EventEmitter();
+        this.post = new FormGroup({
+            'usuario': new FormControl(),
+            'contenido': new FormControl('', [Validators.required, Validators.maxLength(10)]),
+            'likes': new FormControl(0),
+
+
+        });
+        // this.postAgregado = new EventEmitter();
     }
 
 
     ngOnInit() {
     }
     createPost() {
-
+        console.log(this.post.value);
     }
     addPost() {
-        this.postAgregado.emit(this.post);
+        /* this.postAgregado.emit(this.post); */
         // let postNew = {
         //     content: this.memeForm.value.description,
         //     likes: 0,
